4cca6d0a9e3f5eb041d7bff3d3cfe31aeaf5afac
Matt Amos <zerebubuth@gmail.com>
2016-10-06 23:05:38 +0100

    Add support for multiple tables.

--- raw_tiles/command.py
+++ raw_tiles/command.py
@@ -5,12 +5,12 @@ import gzip
 import boto3
 
 
-def output_fmt(fmt, conn, z, x, y):
+def output_fmt(fmt, conn, table, z, x, y):
     st_box2d = st_box2d_for_tile(z, x, y)
 
     query = "select osm_id as fid, st_asbinary(way) as wkb, " \
-            "to_json(tags) as json from planet_osm_polygon where " \
-            "way && %s" % st_box2d
+            "to_json(tags) as json from %s where " \
+            "way && %s" % (table, st_box2d)
 
     with conn.cursor() as cur:
         cur.execute(query)
@@ -22,21 +22,22 @@ def output_fmt(fmt, conn, z, x, y):
             fmt.write(fid, wkb, props)
 
 
-def output_io(io, conn, z, x, y):
+def output_io(io, conn, table, z, x, y):
     with gzip.GzipFile(fileobj=io, mode='wb', compresslevel=9) as fh:
         with msgpack_format.write(fh) as fmt:
-            output_fmt(fmt, conn, z, x, y)
+            output_fmt(fmt, conn, table, z, x, y)
 
 
-def output_tile(s3, output_bucket, conn, z, x, y):
+def output_tile(s3, output_bucket, conn, table, z, x, y):
     with tempfile.NamedTemporaryFile() as tmp:
-        output_io(tmp, conn, z, x, y)
-        key = "%d/%d/%d.msgpack.gz" % (z, x, y)
+        output_io(tmp, conn, table, z, x, y)
+        key = "%s/%d/%d/%d.msgpack.gz" % (table, z, x, y)
         s3.meta.client.upload_file(tmp.name, output_bucket, key)
 
 
 if __name__ == '__main__':
     import sys
+    tables = ['planet_osm_point', 'planet_osm_line', 'planet_osm_polygon']
 
     db_params = sys.argv[1]
     z = int(sys.argv[2])
@@ -49,4 +50,5 @@ if __name__ == '__main__':
     max_coord = 1 << z
     for x in range(0, max_coord):
         for y in range(0, max_coord):
-            output_tile(s3, output_bucket, conn, z, x, y)
+            for table in tables:
+                output_tile(s3, output_bucket, conn, table, z, x, y)
