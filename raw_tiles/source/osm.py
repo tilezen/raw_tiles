594377b0638d4c388dc6caa0e1334cffd367ac27
Matt Amos <zerebubuth@gmail.com>
2017-07-04 18:45:58 +0100

    Add support for ways table.
    We need the ways table for figuring out if a node is used in a way, which is part of the importance calculation for railway stations as well as gates.

--- raw_tiles/source/osm.py
+++ raw_tiles/source/osm.py
@@ -11,27 +11,27 @@ class OsmSource(object):
         self.table_prefix = table_prefix
         self.dbparams = dbparams
 
-    def _write_base_tables(self, cur, make_file_fn, st_box2d):
-        base_table = self.env.get_template('base_table.sql')
-
-        for suffix in ('point', 'line', 'polygon'):
-            table = self.table_prefix + suffix
-            query = base_table.render(table=table, box=st_box2d)
-            cur.execute(query)
-
-            with make_file_fn(table) as fh:
-                for row in cur:
-                    fh.write(*row)
-
-    def _write_relations(self, cur, make_file_fn, st_box2d):
-        relations = self.env.get_template('relations.sql')
-        query = relations.render(box=st_box2d)
+    def _write_table(self, cur, make_file_fn, template, table, st_box2d=None):
+        tmp = self.env.get_template(template)
+        query = tmp.render(table=table, box=st_box2d)
         cur.execute(query)
 
-        table = 'planet_osm_rels'
         with make_file_fn(table) as fh:
             for row in cur:
                 fh.write(*row)
+
+    def _write_base_tables(self, cur, make_file_fn, st_box2d):
+        for suffix in ('point', 'line', 'polygon'):
+            table = self.table_prefix + suffix
+            self._write_table(cur, make_file_fn, 'base_table.sql', table,
+                              st_box2d)
+
+    def _write_ways(self, cur, make_file_fn):
+        self._write_table(cur, make_file_fn, 'ways.sql', 'planet_osm_ways')
+
+    def _write_relations(self, cur, make_file_fn):
+        self._write_table(cur, make_file_fn, 'relations.sql',
+                          'planet_osm_rels')
 
     def write(self, sink, tile):
         st_box2d = tile.box2d()
@@ -45,4 +45,12 @@ class OsmSource(object):
 
         with conn.cursor() as cur:
             self._write_base_tables(cur, make_file, st_box2d)
-            self._write_relations(cur, make_file, st_box2d)
+
+            # set up temporary tables that relations and ways will both need
+            # and share info.
+            tmp = self.env.get_template('setup.sql')
+            query = tmp.render(box=st_box2d)
+            cur.execute(query)
+
+            self._write_ways(cur, make_file)
+            self._write_relations(cur, make_file)
